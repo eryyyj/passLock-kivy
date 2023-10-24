7711af7599ad3b1ea0c3dc237f862ba6eaa00635
Ery Jay Pisalbon <pisalboneryjay@gmail.com>
2023-10-24 15:15:37 +0800

    passlockkivy gui version 1

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.stacklayout import StackLayout
+
+
+class LoginPage(AnchorLayout):
+    pass
+class CreateAccountPage(AnchorLayout):
+    pass
+class SidebarPage(BoxLayout):
+    pass
+class PassLockApp(App):
+    pass
+
+PassLockApp().run()
