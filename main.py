0fda6806eb5fa3872091333eedbb9dce470e250b
Ery Jay Pisalbon <pisalboneryjay@gmail.com>
2023-10-28 17:00:39 +0800

    V2.1 (updated pages)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.app import MDApp
+from kivymd.uix.anchorlayout import MDAnchorLayout
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.stacklayout import StackLayout
@@ -11,7 +12,9 @@ class LoginPage(AnchorLayout):
     pass
 class CreateAccountPage(AnchorLayout):
     pass
-class SidebarPage(BoxLayout):
+class SidebarPage(AnchorLayout):
+    pass
+class MainAppPage(MDAnchorLayout):
     pass
 class PassLockApp(MDApp):
     pass
