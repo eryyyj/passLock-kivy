6a47e3984af6338cb7735b394422a1147211a8b1
Ery Jay Pisalbon <pisalboneryjay@gmail.com>
2023-11-03 17:14:51 +0800

    V4 (added navigation drawer to main page)

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 from kivymd.uix.anchorlayout import MDAnchorLayout
-from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.stacklayout import StackLayout
 from kivy.uix.pagelayout import PageLayout
 from kivy.uix.recycleview import RecycleView
+from kivy.core.window import Window
+from kivymd.uix.navigationdrawer import MDNavigationDrawer
+from kivymd.theming import ThemeManager
+from kivy.uix.scrollview import ScrollView
 
-class LoginPage(AnchorLayout):
-    pass
+Window.size = (300,500)
 class CreateAccountPage(AnchorLayout):
     pass
-class SidebarPage(AnchorLayout):
+pass
+class LoginPage(AnchorLayout):
     pass
 class passList(RecycleView):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.data = [{'text':str(i)} for i in range(20)]
-class PasslockPage(MDAnchorLayout):
+class PasslistPage(MDAnchorLayout):
     pass
-class MainAppPage(PageLayout):
+class SidebarPage(AnchorLayout):
     pass
+class ToolbarMainAppPage(Screen):
+    pass
+class MainAppPage(Screen):
+    pass
+class NavDrawer(MDNavigationDrawer):
+    pass
+
 class PassLockApp(MDApp):
-    pass
+    def navigation_draw(self):
+        print('navigation_draw working')
 
 PassLockApp().run()
