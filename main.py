2f675d450463678eea5b28a92e7cf31c22ad4995
Ery Jay Pisalbon <115686289+eryyyj@users.noreply.github.com>
2023-11-13 22:51:48 +0800

    V6
    Added Add Credentials Page

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
-from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.stacklayout import StackLayout
-
-
-class LoginPage(AnchorLayout):
-    pass
+from kivymd.uix.anchorlayout import MDAnchorLayout
+from kivy.uix.pagelayout import PageLayout
+from kivy.uix.recycleview import RecycleView
+from kivy.core.window import Window
+from kivymd.uix.navigationdrawer import MDNavigationDrawer
+from kivymd.theming import ThemeManager
+from kivy.uix.scrollview import ScrollView
+Window.size = (300,500)
 class CreateAccountPage(AnchorLayout):
     pass
-class SidebarPage(BoxLayout):
+class LoginPage(AnchorLayout):
     pass
+class passList(RecycleView):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.data = [{'text':str(i)} for i in range(20)]
+class PasslistPage(MDAnchorLayout):
+    pass
+class SidebarPage(AnchorLayout):
+    pass
+class ToolbarMainAppPage(Screen):
+    pass
+class MainAppPage(Screen):
+    pass
+class NavDrawer(MDNavigationDrawer):
+    pass
+class ForgotPasswordPage(MDAnchorLayout):
+    pass
+class ForgotPasswordGridLayoutDesign(AnchorLayout):
+    pass
+class CreateAccountSuccessfulPage(AnchorLayout):
+    pass
+class AddCredentialsPage(AnchorLayout):
+    pass
+
 class PassLockApp(MDApp):
-    pass
+    def navigation_draw(self):
+        print('navigation_draw working')
 
 PassLockApp().run()
