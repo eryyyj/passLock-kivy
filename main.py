483435f714cae84036fc5dc0ac4593ad7b24a6b5
Hans <149172680+hansRV17@users.noreply.github.com>
2023-11-15 19:41:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,12 +20,15 @@ class CreateAccountPage(AnchorLayout, Screen):
         email = self.ids.email_input.text
         password_create = self.ids.password_create_input.text
         confirm_password = self.ids.confirm_password_input.text
-
-        if password_create == confirm_password:
-            print("Account created successfully!")
-            self.manager.current = "LoginPage"
+       
+        if password_create and confirm_password:
+            if password_create == confirm_password:
+              #  print("Account created successfully!")
+                self.manager.current = "SuccesfulPage"
+            else:
+                print("Account creation failed. Please check your information.")
         else:
-            print("Account creation failed. Please check your information.")
+            print("Please fill in all fields.")            
 
     def confirm_account(self,username_create, email, password_create):
         confirm_password = "admin"
@@ -51,12 +54,15 @@ class LoginPage(AnchorLayout, Screen):
     def login_clicked(self, instance):
         username_login = self.ids.username_input.text
         password_login = self.ids.password_input.text
-
-        if self.check_credentials(username_login, password_login):
-            print(f"Login successful! Welcome to PassLock, {username_login}.")
-            self.manager.current = "MainPage"
+        
+        if username_login and password_login:
+            if self.check_credentials(username_login, password_login):
+            # print(f"Login successful! Welcome to PassLock, {username_login}.")
+                self.manager.current = "MainPage"
+            else:
+                print("Login failed. Invalid username or password.")
         else:
-            print("Login failed. Invalid username or password.")
+            print("Please fill in all fields.")
 
     def check_credentials(self, username, password):
         check_username = "admin"
@@ -64,25 +70,53 @@ class LoginPage(AnchorLayout, Screen):
 
         return username == check_username and password == check_password
 
-class passList(RecycleView):
+class passList(RecycleView,Screen):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.data = [{'text':str(i)} for i in range(20)]
 class PasslistPage(MDAnchorLayout):
     pass
+
 class SidebarPage(AnchorLayout,Screen):
     pass
+
 class ToolbarMainAppPage(Screen):
     pass
+
 class MainAppPage(Screen):
     pass
+
 class NavDrawer(MDNavigationDrawer):
     pass
+
 class ForgotPasswordPage(MDAnchorLayout,Screen):
-    pass
+    pass   
+
 class ForgotPasswordGridLayoutDesign(AnchorLayout,Screen):
+    def confirm_clicked(self,instance):
+        email_send = self.ids.email_sent.text
+        
+        if email_send:
+            if email_send:
+                print("Email has sent")
+            else:
+                print("Email not found. pelase verify")
+        else:
+            print("Please fill in all fields.")
+class CreateAccountSuccessfulPage(AnchorLayout,Screen):
     pass
 
+class AddCredentialsPage(AnchorLayout,Screen):
+    def add_credential(self,instance):
+        platform = self.ids.platform_add.text
+        add_username = self.ids.username_add.text
+        add_password = self.ids.password_add.text
+        
+        if platform and add_username and add_password:
+            print("Add SuccessFul!!!")
+        else:
+            print("Please fill in all fields")
+    
 class PassLockApp(MDApp):
     def navigation_draw(self):
         print('navigation_draw working')
